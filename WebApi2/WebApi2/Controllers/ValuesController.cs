fc9785d86eb273541cfc9759df8d2adf1321d5be
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-12-04 02:58:11 +0100

    WebApi2 added

--- WebApi2/WebApi2/Controllers/ValuesController.cs
+++ WebApi2/WebApi2/Controllers/ValuesController.cs
@@ -9,6 +9,62 @@ namespace WebApi2.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+
+        [Route("breakfast")]
+        [HttpGet]
+        public string Breakfast(string breakfast)
+        {
+
+            string message;
+
+            if (String.IsNullOrEmpty(breakfast))
+                return "You did not enter anything";
+
+            switch (breakfast.ToLower())
+            {
+                case "egg":
+                    message = "Å nej, du borde inte äta ägg till frukost!";
+                    break;
+                case "mango":
+                    message = "Ja, mango är gott!";
+                    break;
+                default:
+                    message = "Does not exist";
+                    break;
+            }
+
+            return message;
+        }
+
+        [Route("square")]
+        [HttpGet]
+        public int Square(int number)
+        {
+            return number * number;
+        }
+
+        [Route("range")]
+        [HttpGet]
+        public string Range(int fromNumber, int toNumber)
+        {
+            string message = "";
+
+            for (int i = fromNumber; i <= toNumber; i++)
+            {
+                message += i.ToString() + ", ";
+            }
+
+            return message; ;
+        }
+
+        [Route("backgroundcolor")]
+        [HttpGet]
+        public IActionResult BackgroundColor(string backgroundColor)
+        {
+            string html = $"<body style='background-color:{backgroundColor}'></body>";
+            return Content(html, "text/html");
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
